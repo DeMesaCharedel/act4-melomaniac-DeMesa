e116a0c9937ea7d1da8f0a8f2e834d263b606ee2
DeMesaCharedel <charedeldemesa631@gmail.com>
2022-04-17 22:29:48 +0800

    song 1 final

---
+++ song1.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:lightgray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> TimbreList</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to TimbreList</h1>
+                <p class="lead">A song that touches your souls</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+
+
+                        <?php
+                        echo "<h2 class='text-dark'>Safe</h2>";
+                        echo "<p class='lead '>Victory Worship</p>";
+
+                        echo "<h5><pre>A                             </h5>";
+                        echo "<p> Under Your grace, Your mercy amazes me</p>";
+                        echo "<h5><pre>A                             </h5>";
+                        echo "<p> Under Your wings, Your shadow covers me</p>";
+                        echo "<h5><pre>F#M                                A</h5>";   
+                        echo "<p> Your promise of love, where my heart is safely undone</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A                             </h5>";
+                        echo "<p>Speak to me Lord, Your servant is listening</p>";
+                        echo "<h5><pre>F#m                         </h5>";
+                        echo "<p>Over the noise, I hear You whispering</p>";
+                        echo "<h5><pre>D                               A</h5>";
+                        echo "<p>My hope has come and my heart is safely undone</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A                  </h5>";
+                        echo "<p>I found my fortress, in You</p>";
+                        echo "<h5><pre>F#m                      </h5>";
+                        echo "<p>And my soul is anchored, with You</p>";
+                        echo "<h5><pre>D                     E</h5>";
+                        echo "<p>My resting place, is in Your name</p>";
+                        echo "<h5><pre>A        </h5>";
+                        echo "<p>Forever safe</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A                             </h5>";
+                        echo "<p>Speak to me Lord, Your servant is listening</p>";
+                        echo "<h5><pre>F#m                         </h5>";
+                        echo "<p>Over the noise, I hear You whispering</p>";
+                        echo "<h5><pre>D                                     A</h5>";
+                        echo "<p>My hope has come and my heart is safely undone, oh</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A                  </h5>";
+                        echo "<p>I found my fortress, in You</p>";
+                        echo "<h5><pre>F#m                      </h5>";
+                        echo "<p>And my soul is anchored, with You</p>";
+                        echo "<h5><pre>D                     E</h5>";
+                        echo "<p>My resting place, is in Your name</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A                  </h5>";
+                        echo "<p>I found my fortress, in You</p>";
+                        echo "<h5><pre>F#m                      </h5>";
+                        echo "<p>And my soul is anchored, with You</p>";
+                        echo "<h5><pre>D                     E</h5>";
+                        echo "<p>My resting place, is in Your name</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A        </h5>";
+                        echo "<p>Forever safe</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>D                </h5>";
+                        echo "<p>You are never far away</p>";
+                        echo "<h5><pre>A                   </h5>";
+                        echo "<p>Always reaching out to save</p>";
+                        echo "<h5><pre>D                           </h5>";
+                        echo "<p>My weakness covered by Your strength</p>";
+                        echo "<h5><pre>A                   </h5>";
+                        echo "<p>And I am found forever safe</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A                  </h5>";
+                        echo "<p>I found my fortress, in You</p>";
+                        echo "<h5><pre>F#m                      </h5>";
+                        echo "<p>And my soul is anchored, with You</p>";
+                        echo "<h5><pre>D                     E</h5>";
+                        echo "<p>My resting place, is in Your name</p>";
+                        echo "<h5><pre>A        </h5>";
+                        echo "<p>Forever safe</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A                  </h5>";
+                        echo "<p>I found my fortress, in You</p>";
+                        echo "<h5><pre>F#m                      </h5>";
+                        echo "<p>And my soul is anchored, with You</p>";
+                        echo "<h5><pre>D                     E</h5>";
+                        echo "<p>My resting place, is in Your name</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>A                  </h5>";
+                        echo "<p>I found my fortress, in You</p>";
+                        echo "<h5><pre>F#m                      </h5>";
+                        echo "<p>And my soul is anchored, with You</p>";
+                        echo "<h5><pre>D                     E</h5>";
+                        echo "<p>My resting place, is in Your name</p>";
+                        echo "<h5><pre>A        </h5>";
+                        echo "<p>Forever safe</p>";
+                        echo "<br/>";
+
+                        ?>
+                         Back to main page <a href = "index.php"> here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charedel O. De Mesa</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
