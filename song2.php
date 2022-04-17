72d6009af968a37ad339903a6917ff519a2681a6
DeMesaCharedel <charedeldemesa631@gmail.com>
2022-04-17 22:36:15 +0800

    song 2 final

---
+++ song2.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:lightgray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> TimbreList</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to TimbreList</h1>
+                <p class="lead">A song that touches your souls</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+
+                        <?php
+                        echo "<h2 class='text-dark'>Fall</h2>";
+                        echo "<p class='lead '>Hillsong UNITED</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Verse]</p>";
+                        echo "<h5><pre>D                G    </h5>";
+                        echo "<p>I love to worship You, my God</p>";
+                        echo "<h5><pre>D                A    </h5>";
+                        echo "<p>I love to worship You, my Lord</p>";
+                        echo "<h5><pre>     G          D       </h5>";
+                        echo "<p>And see Your Spirit fall in power</p>";
+                        echo "<h5><pre>              G                  </h5>";
+                        echo "<p>Your love unfolding</p>";
+                        echo "<h5><pre> A         </h5>";
+                        echo "<p>Gifts from heaven</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>D                G    </h5>";
+                        echo "<p>I love to worship You, my God</p>";
+                        echo "<h5><pre>D                A    </h5>";
+                        echo "<p>I love to worship You, my Lord</p>";
+                        echo "<h5><pre>                G                        </h5>";
+                        echo "<p>And feel Your precious</p>";
+                        echo "<h5><pre>        D                 </h5>";
+                        echo "<p>Breath of heaven</p>";
+                        echo "<h5><pre>         G           A        </h5>";
+                        echo "<p>Your all consuming love</p>";
+                        echo "<br/>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]</p>";
+                        echo "<h5><pre>D       </h5>";
+                        echo "Holy Spirit come in power";
+                        echo "<h5><pre>  A       </h5>";
+                        echo "<p>Change my heart</p>";
+                        echo "<h5><pre>         Bm            G-A  </h5>";
+                        echo "<p>I want to live for You, my God</p>";
+                        echo "<h5><pre>            D       </h5>";
+                        echo "<p>Let Your Spirit come in power";
+                        echo "<h5><pre>   A     </h5>";
+                        echo "<p>Change my life</p>";
+                        echo "<h5><pre>               Bm         G        </h5>"; 
+                        echo "<p>That I may live for You my Lord</p>";
+                        echo "<h5><pre>A   </h5>";
+                        echo "<p>Fall on us Lord</p>";
+                        echo "<br/>";
+                        echo "<br/>";
+ 
+                        echo "<p>[Bridge]</p>";
+                        echo "<h5><pre>D    </h5>";
+                        echo "<p>So I yearn for You</p>";
+                        echo "<h5><pre>Bm</h5>";
+                        echo "<p>Long to see You move</p>";
+                        echo "<h5><pre>                G                      A            </h5>";   
+                        echo "<p>Lord, I lift my hands before my King and pray</p>";
+                        echo "<br/>";
+
+                        ?>
+                        Back to main page <a href = "index.php"> here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charedel O. De Mesa</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
