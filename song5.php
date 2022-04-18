c877a34e900741a9067952626a6664f47c1786d2
DeMesaCharedel <charedeldemesa631@gmail.com>
2022-04-18 22:43:35 +0800

    song 5 final

---
+++ song5.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:lightgray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> TimbreList</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to TimbreList</h1>
+                <p class="lead">A song that touches your souls</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+                        echo "<h2 class='text-dark'>Tribes</h2>";
+                        echo "<p class='lead '>Victory Worship</p>";
+
+                        echo "<p>[Intro]</p>";
+                        echo "<p>D G D G</p>";
+                        echo "<br/>";
+ 
+                        echo "<p>[Verse]</p>";
+                        echo "<h5><pre>D                   G </h5>";
+                        echo "<p>Every tribe will see Your glory</p>";
+                        echo "<h5><pre>D               G      </h5>";
+                        echo "<p>Every nation bow before You</p>";
+                        echo "<h5><pre>D                   G  </h5>";
+                        echo "<p>All our treasure turned to ashes</p>";
+                        echo "<h5><pre>                D/F# G</h5>";
+                        echo "<p>In the light of You</p>";
+                        echo "<br/>";
+ 
+ 
+                        echo "<p>[Pre-Chorus]</p>";
+                        echo "<h5><pre>G  D/F#   Em D</h5>";
+                        echo "<p>As we're singing</p>";
+                        echo "<h5><pre>A               Bm  </h5>";
+                        echo "<p>Holy is the Lord Almighty</p>";
+                        echo "<h5><pre>             A     </h5>";
+                        echo "<p>Only You are worthy</p>";
+                        echo "<h5><pre>              G      </h5>";
+                        echo "<p>Worthy of it all</p>";
+                        echo "<br/>";
+ 
+ 
+                        echo "<p>[Verse]</p>";
+                        echo "<h5><pre>D                   G  </h5>";
+                        echo "<p>Every tribe will see Your glory</p>";
+                        echo "<h5><pre>D                G</h5>";
+                        echo "<p>Every nation bow before You, ohh-oh-oh-ohh</p>";
+                        echo "<h5><pre>D                   G  </h5>";
+                        echo "<p>All our treasure turned to ashes</p>";
+                        echo "<h5><pre>D/F#       G   </h5>";
+                        echo "<p>In the light of You, oh, ohh-oh-oh-ohh</p>";
+                        echo "<br/>";
+ 
+ 
+                        echo "<p>[Pre-Chorus]</p>";
+                        echo "<h5><pre>G  D/F#   Em D</h5>";
+                        echo "<p>As we're singing</p>";
+                        echo "<h5><pre>A              Bm</h5>";
+                        echo "<p>Holy is the Lord Almighty</p>";
+                        echo "<h5><pre>              A   </h5>";
+                        echo "<p>Only You are worthy</p>";
+                        echo "<h5><pre>              G     </h5>";
+                        echo "<p>Worthy of it all</p>";
+                        echo "<br/>";
+ 
+ 
+                        echo "<p>[Chorus]</p>";
+                        echo "<h5><pre>D          </h5>";
+                        echo "<p>Praise and glory</p>";
+                        echo "<h5><pre>D            </h5>";
+                        echo "<p>Honor and strength</p>";
+                        echo "<h5><pre>G          </h5>";
+                        echo "<p>Unto our God</p>";
+                        echo "<h5><pre>G          </h5>";
+                        echo "<p>Unto our God</p>";
+                        echo "<h5><pre>Bm         </h5>";
+                        echo "<p>Matchless, endless</p>";
+                        echo "<h5><pre>Bm         </h5>";
+                        echo "<p>Love unrestrained</p>";
+                        echo "<h5><pre>G          </h5>";
+                        echo "<p>This is our God</p>";
+                        echo "<h5><pre>G          </h5>";
+                        echo "<p>Every tribe sing</p>";
+                        echo "<h5><pre>D           </h5>";
+                        echo "<p>Praise and glory</p>";
+                        echo "<h5><pre>D           </h5>";
+                        echo "<p>Honor and strength</p>";
+                        echo "<h5><pre>G          </h5>";
+                        echo "<p>Unto our God</p>";
+                        echo "<h5><pre>G           </h5>";
+                        echo "<p>Unto our God</p>";
+                        echo "<h5><pre>Bm          </h5>";
+                        echo "<p>Matchless, endless</p>";
+                        echo "<h5><pre>Bm         </h5>";
+                        echo "<p>Love unrestrained</p>";
+                        echo "<h5><pre>G           </h5>";
+                        echo "<p>This is our God</p>";
+                        echo "<h5><pre>G           </h5>";
+                        echo "<p>Every tribe sing</p>";
+                        echo "<br/>";
+ 
+ 
+                        echo "<p>[Post-Chorus]</p>";
+                        echo "<h5><pre>D            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>G            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa We sing</p>";
+                        echo "<h5><pre>D            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>G             </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa Oh-ohh</p>";
+                        echo "<br/>";
+ 
+ 
+                        echo "<p>[Bridge]</p>";
+                        echo "<h5><pre>D                </h5>";
+                        echo "<p>There is no one like our God</p>";
+                        echo "<p>(No one)</p>";
+                        echo "<h5><pre>G                </h5>";
+                        echo "<p>There is no one like our God</p>";
+                        echo "<h5><pre>D                </h5>";
+                        echo "<p>There is no one like our God</p>";
+                        echo "<h5><pre>G                   </h5>";
+                        echo "<p>There is no one like our God</p>";
+                        echo "<h5><pre>D                   </h5>";
+                        echo "<p>Wala kang katulad O Dios</p>";
+                        echo "<h5><pre>G                    </h5>";
+                        echo "<p>Wala kang katulad O Dios</p>";
+                        echo "<h5><pre>D                    </h5>";
+                        echo "<p>No hay nadie como nuestro Dios</p>";
+                        echo "<h5><pre>G                    </h5>";
+                        echo "<p>No hay nadie como nuestro Dios</p>";
+                        echo "<h5><pre>D                    </h5>";
+                        echo "<p>There is no one like our God</p>";
+                        echo "<p>(We sing)</p>";
+                        echo "<h5><pre>G                    </h5>";
+                        echo "<p>There is no one like our God</p>";
+                        echo "<h5><pre>Bm                   </h5>";
+                        echo "<p>There is no one like our God</p>";
+                        echo "<h5><pre>G                    </h5>";
+                        echo "<p>There is no one like our God</p>";
+                        echo "<br/>";
+ 
+ 
+                        echo "<p>[Chorus]</p>";
+                        echo "<h5><pre>D            </h5>";
+                        echo "<p>Praise and glory</p>";
+                        echo "<h5><pre>D            </h5>";
+                        echo "<p>Honor and strength</p>";
+                        echo "<h5><pre>G             </h5>";
+                        echo "<p>Unto our God</p>";
+                        echo "<h5><pre>G             </h5>";
+                        echo "<p>Unto our God</p>";
+                        echo "<h5><pre>Bm              </h5>";
+                        echo "<p>You're matchless, endless</p>";
+                        echo "<h5><pre>Bm               </h5>";
+                        echo "<p>Love unrestrained</p>";
+                        echo "<h5><pre>G                </h5>";
+                        echo "<p>This is our God</p>";
+                        echo "<h5><pre>G                </h5>";
+                        echo "<p>Every tribe sing</p>";
+                        echo "<h5><pre>D                </h5>";
+                        echo "<p>Praise and glory</p>";
+                        echo "<h5><pre>D                </h5>";
+                        echo "<p>Honor and strength</p>";
+                        echo "<h5><pre>G                </h5>";
+                        echo "<p>Unto our God</p>";
+                        echo "<h5><pre>G                </h5>";
+                        echo "<p>Unto our God</p>";
+                        echo "<h5><pre>Bm               </h5>";
+                        echo "<p>You're matchless, endless</p>";
+                        echo "<h5><pre>Bm               </h5>";
+                        echo "<p>Love unrestrained</p>";
+                        echo "<h5><pre>G                </h5>";
+                        echo "<p>This is our God</p>";
+                        echo "<h5><pre>G                </h5>";
+                        echo "<p>This is our God</p>";
+                        echo "<br/>";
+ 
+ 
+                        echo "<p>[Outro]</p>";
+                        echo "<h5><pre>D            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>G            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>D            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>G            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>D            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>G            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>D                 </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<h5><pre>G            </h5>";
+                        echo "<p>Ayo-ayo-ayo-waa</p>";
+                        echo "<br/>";
+
+                        ?>
+                         Back to main page <a href = "index.php"> here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charedel O. De Mesa</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
