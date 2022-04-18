cd1c8ef869273b78f94d2c560be7bb4602e2918f
DeMesaCharedel <charedeldemesa631@gmail.com>
2022-04-18 16:03:07 +0800

    song 4 final

---
+++ song4.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:lightgray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+          
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> TimbreList</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to TimbreList</h1>
+                <p class="lead">A song that touches your souls</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+                        echo "<h2 class='text-dark'>Blessings</h2>";
+                        echo "<p class='lead '>Laura Story</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>[Instro]<p>";
+                        echo "<h5><pre>Am  G  D/F#  C  D  Bm</h5>";
+                        echo "<h5><pre>Am  G  D/F#  C</h5>";
+                        echo "<br/>";
+                        echo "<br/>";
+
+                        echo "<p>[Verse 1]</p>";
+                        echo "<h5><pre>C    D    Em    </h5>";
+                        echo "<p>We pray for blessings</p>";
+                        echo "<h5><pre>  C    D    Bm    </h5>";
+                        echo "<p>We pray for peace</p>";
+                        echo "<h5><pre>  C     D    Em      F           C    </h5>";
+                        echo "<p>Comfort for family, protection while we sleep</p>";
+                        echo "<h5><pre>  C    D    Em   </h5>";
+                        echo "<p>We pray for healing</p>";
+                        echo "<h5><pre>  C     D    Bm</h5>";
+                        echo "<p>For prosperity</p>";
+                        echo "</h5><pre>C      D       Em               Am           D</h5>";
+                        echo "<p>We pray for Your mighty hand to ease our suffering</p>";
+                        echo "<h5><pre>           G     C         D          Em            </h5>";
+                        echo "<p>All the while, You hear each spoken need</p>";
+                        echo "<h5><pre>  Am          G              D       </h5>";
+                        echo "<p>Yet love us way too much to give us lesser things<p>";
+                        echo "<br/>";
+                        echo "<br/>";
+    
+                        echo "<p>[Chorus]</p>";
+                        echo "<h5><pre>G             </h5>";
+                        echo "<p>What if your blessings come through raindrops</p>";
+                        echo "<h5><pre>D           </h5>";
+                        echo "<p>What if Your healing comes through tears</p>";
+                        echo "<h5><pre>Em              D               C        </h5>";
+                        echo "<p>What if the thousand sleepless nights are what it takes to know You're near</p>";
+                        echo "<h5><pre>                  Am           G             C          D                 Am G D    C D Bm    </h5>";
+                        echo "<p>What if trials of this life are Your mercies in disguise</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Verse 2]</p>";
+                        echo "<h5><p>C     D      Em</h5>";
+                        echo "<p>We pray for wisdom</p>";
+                        echo "<h5><pre>C      D       Bm</h5>";
+                        echo "<p>Your voice to hear</p>";
+                        echo "<h5><pre>        C    D       Em           F            C   </h5>";
+                        echo "<p>We cry in anger when we cannot feel You near</p>";
+                        echo "<h5><pre>C   D         Em   </h5>";
+                        echo "<p>We doubt your goodness</p>";
+                        echo "<h5><pre>      C    D         Bm      </h5>";
+                        echo "<p>We doubt your love</p>";
+                        echo "<h5><pre>    C    D     Em            Am           D      </h5>";
+                        echo "<p>As if every promise from Your Word is not enough</p>";
+                        echo "<h5><pre>     G        C         D                   Em      </h5>";
+                        echo "<p>And all the while, You hear each desperate plea</p>";
+                        echo "<h5><pre>      Am                G        D     </h5>";
+                        echo "<p>And long that we'd have faith to believe</p>";
+                        echo "<br/>";
+     
+                        echo "<p>[Chorus]</p>";
+                        echo "<h5><pre>G             </h5>";
+                        echo "<p>What if your blessings come through raindrops</p>";
+                        echo "<h5><pre>D           </h5>";
+                        echo "<p>What if Your healing comes through tears</p>";
+                        echo "<h5><pre>Em              D               C        </h5>";
+                        echo "<p>What if the thousand sleepless nights are what it takes to know You're near</p>";
+                        echo "<h5><pre>                  Am           G             C          D                 Am G D    C D Bm    </h5>";
+                        echo "<p>What if trials of this life are Your mercies in disguise</p>";
+                        echo "<br/>";
+     
+                        echo "<p>[Bridge]</p>";
+                        echo "<h5><pre>Am             G    </h5>";
+                        echo "<p>When friends betray us</p>";
+                        echo "<h5><pre>     C          D          Am      </h5>";
+                        echo "<p>When darkness seems to win we know<p>";
+                        echo "<h5><pre>        G                  C       </h5>";
+                        echo "<p>The pain reminds this heart That this is not</p>";
+                        echo "<h5><pre>                       D         Am  G  D     C  D  Bm      </h5>";
+                        echo "<p>This is not our home</p>";
+                        echo "<h5><pre>                                 C  D  Em  F  C             </h5>";
+                        echo "<p>It's not our home</p>";
+                        echo "<br/>";
+     
+                        echo "<p>[Chorus 2]</p>";
+                        echo "<h5><pre>G             </h5>";
+                        echo "<p>What if your blessings come through raindrops</p>";
+                        echo "<h5><pre>D           </h5>";
+                        echo "<p>What if Your healing comes through tears</p>";
+                        echo "<h5><pre>Em              D               C        </h5>";
+                        echo "<p>What if the thousand sleepless nights are what it takes to know You're near</p>";
+                        echo "<h5><pre>         G             </h5>";
+                        echo "<p>What if my greatest disappointments</p>";
+                        echo "<h5><pre>   D       </h5>";
+                        echo "<p>Or the aching of this life</p>";
+                        echo "<h5><pre>           Em        D              C                 </h5>";
+                        echo "<p>Is the revealing of a greater thirst this world can't satisfy</p>";
+                        echo "<h5><pre>    Am      G          C            D                   Em        </h5>";
+                        echo "<p>What if trials of this life, the rain, the storms, the hardest nights</h5>";
+                        echo "<h5><pre>                      Am       D                  Am G D    C D Bm G</h5>";
+                        echo "<p>Are your mercies in disguise</p>";
+                        echo "<br/>";
+                        echo "<br/>";
+                        ?>Back to main page <a href = "index.php"> here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charedel O. De Mesa</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
