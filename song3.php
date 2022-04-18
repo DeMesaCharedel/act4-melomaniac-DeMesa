53b76b4eb81c2785e9c5e963c5be533bd66eae7b
DeMesaCharedel <charedeldemesa631@gmail.com>
2022-04-18 12:49:27 +0800

    song 3 final

---
+++ song3.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:lightgray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> TimbreList</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to TimbreList</h1>
+                <p class="lead">A song that touches your souls</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+                        echo "<h2 class='text-dark'>Captivate Us</h2>";
+                        echo "<p class='lead '>Watermark</p>";
+                        echo "<br/>";
+
+
+                        echo "<p>[Instrumental]<p>";
+                        echo "<h5><pre>(Em)  D  C</h5>";
+                        echo "<h5><pre>Bb6  Am7  G</h5>";
+                        echo "<h5><pre>(Em)  D  C</h5>";
+                        echo "<h5><pre>Bb6  Am7  G</h5>";
+                        echo "<br/>";
+                        echo "<br/>";
+
+                        echo "<p>[Verse 1]</p>";
+                        echo "<h5><pre>                G           D/F#                 Em      Bm7           </h5>";
+                        echo "<p>Your face is beautiful and Your eyes are like the stars</p>";
+                        echo "<h5><pre>                C              G     C                D    C        </h5>";
+                        echo "<p>Your gentle hands have healing there inside the scars</p>";
+                        echo "<h5><pre>    G              D/F#           </h5>";
+                        echo "<p>Your loving arms they draw me near</p>";
+                        echo "<h5><pre>             Em              Bm7      </h5>";
+                        echo "<p>And Your smile it brings me peace</p>";
+                        echo "<h5><pre>        C         G                C  D    G  C/G  G    </h5>";
+                        echo "<p>Draw me closer oh my Lord, draw me closer Lord To Thee</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]<p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>         C             D   B7             Em     </h5>";
+                        echo "<p>Captivate us, Lord Jesus, set our eyes on You</p>";
+                        echo "<h5><pre>          C                D           G C/G  G     </h5>";
+                        echo "<p>Devastate us with Your presence falling down</p>";
+                        echo "<h5><pre>                C          D         </h5>";
+                        echo "<p>And rushing river, draw us nearer</p>";
+                        echo "<h5><pre>         B7                   Em    </h5>";
+                        echo "<p>Holy fountain consume us with You</p>";
+                        echo "<h5><pre>         C             D        Em </h5>";
+                        echo "<p>Captivate us Lord Jesus, with You</p>";
+                        echo "<br/>";
+                        echo "<br/>";
+
+                        echo "<p>[Instrumental]<p>";
+                        echo "<h5><pre>(Em)  D  C</h5>";
+                        echo "<h5><pre>Bb6  Am7  G</h5>";
+                        echo "<br/>";
+                        echo "<br/>";
+ 
+                        echo "<p>[Verse 2]</p>";
+                        echo "<h5><pre>         G        D/F#            Em7            Bm7       </h5>";
+                        echo "Your voice is powerful and Your words are radiant bright";
+                        echo "<h5><pre>         C                G           C        D  C        </h5>";
+                        echo "<p>In Your breath and shadow I will come close and abide</p>";
+                        echo "<h5><pre>           G             D/F#              Em       Bm7       </h5>";
+                        echo "<p>You whisper love and life divine and Your fellowship is free</p>";
+                        echo "<h5><pre>                C          G              C     D         G C/GG       </h5>";
+                        echo "<p>Draw me closer O my Lord, draw me closer Lord to Thee<p>";
+                        echo "<br/>";
+ 
+                        echo "<p>[Chorus]<p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>         C             D   B7             Em     </h5>";
+                        echo "<p>Captivate us, Lord Jesus, set our eyes on You</p>";
+                        echo "<h5><pre>          C                D           G C/G  G     </h5>";
+                        echo "<p>Devastate us with Your presence falling down</p>";
+                        echo "<h5><pre>                C          D         </h5>";
+                        echo "<p>And rushing river, draw us nearer</p>";
+                        echo "<h5><pre>         B7                   Em    </h5>";
+                        echo "<p>Holy fountain consume us with You</p>";
+                        echo "<h5><pre>         C             D        Em </h5>";
+                        echo "<p>Captivate us Lord Jesus, with You</p>";
+                        echo "<br/>";
+                        echo "<br/>";
+
+                        echo "<p>[Bridge]</p>";
+                        echo "<h5><pre>            C               D             Bb6     D/F# G   </h5>";
+                        echo "<p>Let everything be lost in the shadows of the light of Your face</p>";
+                        echo "<h5><pre>                 C         D          Bb6      D/F#  G    </h5>";
+                        echo "<p>Let every chain be broken from me as I'm bound in Your grace</p>";
+                        echo "<h5><pre>           C             D            </h5>";
+                        echo "<p>For Your yoke is easy, Your burden is light</p>";
+                        echo "<h5><pre>             G      D/F# Em                 </h5>";
+                        echo "<p>You're full of wisdom, power and might</p>";
+                        echo "<h5><pre>       Fmaj7       C  G C  </h5>";
+                        echo "<p>And every eye will see You</p>";
+                        echo "<br/>";
+
+                        echo "<p>[Chorus]<p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>         C             D   B7             Em     </h5>";
+                        echo "<p>Captivate us, Lord Jesus, set our eyes on You</p>";
+                        echo "<h5><pre>          C                D           G C/G  G     </h5>";
+                        echo "<p>Devastate us with Your presence falling down</p>";
+                        echo "<h5><pre>                C          D         </h5>";
+                        echo "<p>And rushing river, draw us nearer</p>";
+                        echo "<h5><pre>         B7                   Em    </h5>";
+                        echo "<p>Holy fountain consume us with You</p>";
+                        echo "<h5><pre>         C             D        Em </h5>";
+                        echo "<p>Captivate us Lord Jesus, with You</p>";
+                        echo "<br/>";
+                        echo "<br/>";
+
+                        echo "<p>[Outro]</p>";
+                        echo "<h5><pre>Em  D  C</h5>";
+                        echo "<h5><pre>Bb6  Am7  G</h5>";
+                        echo "<h5><pre>Em  D  C</h5>";
+                        echo "<h5><pre>Bb6  Am7  G</h5>";
+                        echo "<br/>";
+                        ?>
+                        Back to main page <a href = "index.php"> here!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charedel O. De Mesa</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
